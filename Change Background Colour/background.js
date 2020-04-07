cfd01af54a3039c128e19a5f112a40bfbbef88e9
Dhruv Rawat <dhruv.noida175@gmail.com>
2020-04-08 04:03:26 +0530

    initial commit

---
+++ Change Background Colour/background.js
@@ -0,0 +1,20 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#F5DEB3'}, function() {
+      console.log("The color is wheat.");
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+          conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostEquals: 'developer.chrome.com'},
+          })
+          ],
+              actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+      });
+  });
